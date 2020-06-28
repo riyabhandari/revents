17e7292c26a2926daa1723388c5105c210af8a83
Riya Bhandari <riyabhandari04@gmail.com>
2020-06-28 14:35:15 +0530

    third commit

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Grid, Button } from 'semantic-ui-react'
 import EventList from '../EventList/EventList'
 import EventForm from '../EventForm/EventForm'
+import cuid from 'cuid'
 
 const eventsFromDashboard = [
   {
@@ -64,6 +65,14 @@ class EventDashboard extends Component {
             isOpen:!isOpen
         }))
     }
+    handleCreateEvent=(newEvent)=>{
+        newEvent.id=cuid()
+        newEvent.hostPhotoURL="assets/user.png"
+        this.setState(({events})=>({
+            events:[...events,newEvent],
+            isOpen:false
+        }))
+    }
     render() {
         const {events,isOpen}=this.state
         return (
@@ -73,7 +82,7 @@ class EventDashboard extends Component {
                 </Grid.Column>
                 <Grid.Column width={6}>
                     <Button onClick={this.handleIsOpenToggle} positive content="Create Event"></Button>
-                    {isOpen && <EventForm cancelFormOpen={this.handleIsOpenToggle}/>}
+                    {isOpen && <EventForm createEvent={this.handleCreateEvent} cancelFormOpen={this.handleIsOpenToggle}/>}
                 </Grid.Column>
             </Grid>
         )
--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -2,30 +2,47 @@ import React, { Component } from 'react'
 import { Segment, Form, Button } from 'semantic-ui-react'
 
 class EventForm extends Component {
+    state={
+        title:"",
+        date:"",
+        city:"",
+        venue:"",
+        hostedBy:""
+    }
+    handleSubmit=(evt)=>{
+        evt.preventDefault();
+        this.props.createEvent(this.state)
+    }
+    handleChange=(evt)=>{
+        this.setState({
+            [evt.target.name]:evt.target.value
+        })
+    }
     render() {
         const {cancelFormOpen}=this.props
+        const {title,date,city,venue,hostedBy}=this.state
         return (
                   <Segment>
-                    <Form>
+                    <Form onSubmit={this.handleSubmit} autoComplete="off">
                       <Form.Field>
                         <label>Event Title</label>
-                        <input placeholder="First Name" />
+                        <input name="title" onChange={this.handleChange} value={title} placeholder="Event Title" />
                       </Form.Field>
                       <Form.Field>
                         <label>Event Date</label>
-                        <input type="date" placeholder="Event Date" />
+                        <input name="date" onChange={this.handleChange} value={date} type="date" placeholder="Event Date" />
                       </Form.Field>
                       <Form.Field>
                         <label>City</label>
-                        <input placeholder="City event is taking place" />
+                        <input name="city" onChange={this.handleChange} value={city} placeholder="City event is taking place" />
                       </Form.Field>
                       <Form.Field>
                         <label>Venue</label>
-                        <input placeholder="Enter the Venue of the event" />
+                        <input name="venue" onChange={this.handleChange} value={venue} placeholder="Enter the Venue of the event" />
                       </Form.Field>
                       <Form.Field>
                         <label>Hosted By</label>
-                        <input placeholder="Enter the name of person hosting" />
+                        <input name="hostedBy" onChange={this.handleChange} value={hostedBy} placeholder="Enter the name of person hosting" />
                       </Form.Field>
                       <Button positive type="submit">
                         Submit
